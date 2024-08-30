7b3c2c02e332f30114f3c459f109d36aa154fa26
meirna-kamal <Meirna.abdelrahman99@eng-st.cu.edu.eg>
2024-08-31 01:41:21 +0300

    Edit ClientApplications model with uuid token generations and validations

--- app/models/client_application.rb
+++ app/models/client_application.rb
@@ -1,2 +1,11 @@
 class ClientApplication < ApplicationRecord
+    before_create :generate_token
+
+    validates :name, presence: true, length: { maximum: 255 }
+    validates :token, presence: true, uniqueness: true
+
+    private
+    def generate_token
+        self.token = SecureRandom.uuid
+    end
 end

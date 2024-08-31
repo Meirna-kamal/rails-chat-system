197bde2a8bbfefcd554eaab893f82c6538ee40ea
meirna-kamal <Meirna.abdelrahman99@eng-st.cu.edu.eg>
2024-08-31 13:04:20 +0300

    Edit comments and formatting in ClientApplicationsController

--- app/controllers/client_applications_controller.rb
+++ app/controllers/client_applications_controller.rb
@@ -1,27 +1,34 @@
 class ClientApplicationsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :show]
+
+  
+  # POST /applications
+  def create
+      begin
+        if params[:name].blank?
+          render json: { error: { code: 400, message: "Name is required" } }, status: :bad_request
+          return
+        end
     
-    skip_before_action :verify_authenticity_token, only: [:create, :update, :show]
+        @client_application = ClientApplication.new(name: params[:name])
+    
+        if @client_application.save
+          render json: { data: { token: @client_application.token } }, status: :created
+        else
+          render json: { error: { code: 422, message: "Unable to create application. Please check your input and try again." } }, status: :unprocessable_entity
+        end
 
-    def create
-        begin
-          if params[:name].blank?
-            render json: { error: { code: 400, message: "Name is required" } }, status: :bad_request
-          else
-            @client_application = ClientApplication.new(name: params[:name])
-      
-            if @client_application.save
-              render json: { data: { token: @client_application.token } }, status: :created
-            else
-              render json: { error: { code: 422, message: "Unable to create application. Please check your input and try again." } }, status: :unprocessable_entity
-            end
-        end
-        rescue StandardError => e
-          render json: { error: { code: 500, message: "Internal server error" } }, status: :internal_server_error
-        end
+      rescue StandardError => e
+        render json: { error: { code: 500, message: "Internal server error" } }, status: :internal_server_error
+      end
+
     end
-      
 
-    def show
+    
+  # GET /applications/:application_token
+  def show
+    begin
+
       @client_application = ClientApplication.find_by(token: params[:application_token])
     
       if @client_application
@@ -34,32 +41,40 @@ class ClientApplicationsController < ApplicationController
       else
         render json: { error: { code: 404, message: "Application not found" } }, status: :not_found
       end
+
+    rescue StandardError => e
+      render json: { error: { code: 500, message: "Internal server error" } }, status: :internal_server_error
     end
-      
-    def update
-      # Find the client application by token
+
+  end
+    
+
+  # PATCH /applications/:application_token
+  def update
+    begin
+
       @client_application = ClientApplication.find_by(token: params[:application_token])
     
-      # Check if the application was found
       if @client_application.nil?
         render json: { error: { code: 404, message: "Client application not found" } }, status: :not_found
         return
       end
     
-      # Validate presence of the name parameter
       if params[:name].blank?
         render json: { error: { code: 400, message: "Name is required" } }, status: :bad_request
         return
       end
     
-      # Update the name attribute
       if @client_application.update(name: params[:name])
         render json: { data: { token: @client_application.token, name: @client_application.name } }, status: :ok
       else
         render json: { error: { code: 422, message: "Unable to update application. Please check your input and try again." } }, status: :unprocessable_entity
       end
-    end
-      
-    private
 
+    rescue StandardError => e
+      render json: { error: { code: 500, message: "Internal server error" } }, status: :internal_server_error
+    end
+
+  end
+    
 end

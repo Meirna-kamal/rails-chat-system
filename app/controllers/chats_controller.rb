f0164071f0c21d22bc6f55a4164ef2a8829d5606
meirna-kamal <Meirna.abdelrahman99@eng-st.cu.edu.eg>
2024-08-31 16:38:23 +0300

    Add show chat functionality

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,5 @@
 class ChatsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create, :show]
   
   def create
     begin
@@ -9,9 +9,9 @@ class ChatsController < ApplicationController
             return
         end
 
-        @chat = client_application.chats.build
-        if @chat.save
-            render json: { data: { number: @chat.chat_number } }, status: :created
+        chat = client_application.chats.build
+        if chat.save
+            render json: { data: { number: chat.chat_number } }, status: :created
         else
             render json: { error: { code: 422, message: "Unable to create chat. Please check your input and try again." } }, status: :unprocessable_entity
         end
@@ -19,6 +19,27 @@ class ChatsController < ApplicationController
     rescue StandardError => e
         render json: { error: { code: 500, message: "Internal server error" } }, status: :internal_server_error
     end
-  
   end
+
+
+  def show
+    begin
+        client_application = ClientApplication.find_by(token: params[:application_token])
+        if client_application.nil?
+            render json: { error: { code: 404, message: "Invalid token: Application not found" } }, status: :not_found
+            return
+        end
+
+        chat = client_application.chats.find_by(chat_number: params[:chat_number])
+        if chat
+            render json: { data: { messages_count: chat.messages_count } }, status: :ok
+        else
+            render json: { error: { code: 404, message: "Invalid chat number: Chat not found" } }, status: :not_found
+        end
+    
+    rescue StandardError => e
+        render json: { error: { code: 500, message: "Internal server error" } }, status: :internal_server_error
+    end
+  end
+  
 end

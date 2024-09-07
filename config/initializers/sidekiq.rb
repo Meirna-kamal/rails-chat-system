7a4a60f33bf5811fc07b359bc42d783008a77854
meirna-kamal <Meirna.abdelrahman99@eng-st.cu.edu.eg>
2024-09-07 19:46:26 +0300

    Set up Docker environment for application

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/1' }
+    config.redis = { url: 'redis://redis:6379/1' }
 
     schedule_file = "config/schedule.yml"
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
   
 Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/1' }
+    config.redis = { url: 'redis://redis:6379/1' }
 end
   
